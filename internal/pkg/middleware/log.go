3e8b91713080c7f625813973ab7e9a72e9872def
Arata Yano <arata@stock2shop.com>
2024-10-29 22:37:30 +0200

    log request middleware

---
+++ internal/pkg/middleware/log.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"context"
+	"github.com/Ayano2000/push/internal/types"
+	"github.com/rs/zerolog"
+	"net/http"
+	"time"
+)
+
+func LogRequest(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		// Get the logger from the default context logger
+		log := zerolog.DefaultContextLogger.With().
+			Str("method", r.Method).
+			Str("path", r.URL.Path).
+			Str("remote_ip", r.RemoteAddr).
+			Logger()
+
+		// Store logger in request context
+		ctx := context.WithValue(r.Context(), types.LoggerContextKey, &log)
+		r = r.WithContext(ctx)
+
+		// Call the next handler
+		next.ServeHTTP(w, r)
+
+		// Log the request details after completion
+		log.Info().
+			Dur("duration_ms", time.Since(start)).
+			Msg("Request completed")
+	}
+}
