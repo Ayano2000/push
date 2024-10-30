36b371ede7116405276e70716189ae093c1ea47a
Arata Yano <arata@stock2shop.com>
2024-10-30 14:45:44 +0200

    constants for handler error messages

---
+++ internal/handlers/errors.go
@@ -0,0 +1,7 @@
+package handlers
+
+const getWebhookContentErrorMessage = "Failed to fetch webhook content"
+const getWebhooksErrorMessage = "Failed to fetch webhooks"
+const createWebhookErrorMessage = "Failed to create webhook"
+const invalidJQFilterErrorMessage = "JQ filter is invalid"
+const requestBodyDecodingErrorMessage = "Failed to decode the request body"
